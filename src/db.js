9410ba12526a13c70f7a751084ccc0ac33215b32
nisu <nisu@localhost.localdomain>
2018-07-25 00:29:18 -0500

    changed article order

--- src/db.js
+++ src/db.js
@@ -2,22 +2,6 @@ const dbInit = (db) => {
   const posts = db.addCollection('posts');
   const users = db.addCollection('users');
 
-  const testPost0 = posts.insert({
-    title: 'Test Title',
-    author: 'Nisarg Joshi',
-    createdAt: new Date(),
-    description: 'this is an article',
-    content: 'blah blah blah blah',
-    tags: ['computer science', 'web development'],
-    favorites: 0,
-    comments: [
-      {
-        username: 'billybobjoe',
-        comment: 'i love this'
-      }
-    ]
-  });
-
   const testPost1 = posts.insert({
     title: 'Why I Use React Over Angular',
     author: 'Nisarg Joshi',
@@ -39,6 +23,22 @@ const dbInit = (db) => {
     author: 'Nisarg Joshi',
     createdAt: new Date(),
     description: 'Configuration vs. code, and the effects on modularity.',
+    content: 'blah blah blah blah',
+    tags: ['computer science', 'web development'],
+    favorites: 0,
+    comments: [
+      {
+        username: 'billybobjoe',
+        comment: 'i love this'
+      }
+    ]
+  });
+
+  const testPost0 = posts.insert({
+    title: 'Test Title',
+    author: 'Nisarg Joshi',
+    createdAt: new Date(),
+    description: 'this is an article',
     content: 'blah blah blah blah',
     tags: ['computer science', 'web development'],
     favorites: 0,

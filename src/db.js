f2fe83048f2bc25de608a763c858152cc154adf0
nisu <nisu@localhost.localdomain>
2018-07-24 23:25:47 -0500

    adding some data for frontend testing

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ const dbInit = (db) => {
   const posts = db.addCollection('posts');
   const users = db.addCollection('users');
 
-  const testPost = posts.insert({
+  const testPost0 = posts.insert({
     title: 'Test Title',
     author: 'Nisarg Joshi',
     createdAt: new Date(),
@@ -18,7 +18,39 @@ const dbInit = (db) => {
     ]
   });
 
-  const testUser = users.insert({
+  const testPost1 = posts.insert({
+    title: 'Why I Use React Over Angular',
+    author: 'Nisarg Joshi',
+    createdAt: new Date(),
+    description: 'An all too common question for new web developers.',
+    content: 'blah blah blah blah',
+    tags: ['computer science', 'web development'],
+    favorites: 0,
+    comments: [
+      {
+        username: 'billybobjoe',
+        comment: 'i love this'
+      }
+    ]
+  });
+
+  const testPost2 = posts.insert({
+    title: 'Hapi vs. Express',
+    author: 'Nisarg Joshi',
+    createdAt: new Date(),
+    description: 'Configuration vs. code, and the effects on modularity.',
+    content: 'blah blah blah blah',
+    tags: ['computer science', 'web development'],
+    favorites: 0,
+    comments: [
+      {
+        username: 'billybobjoe',
+        comment: 'i love this'
+      }
+    ]
+  });
+
+  const user = users.insert({
     username: 'nisu',
     password: '$2b$10$F8J8NceC2UOyxsBVTpT6BOSP0P1vygZ.jVrSyHxSqy/sgPu3mevOO'
   });
